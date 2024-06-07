9b14453bad71538033831b9b5585cfe5bab7da26
Qasim Arshad <45349077+mohmdqasim@users.noreply.github.com>
2024-06-07 18:08:12 +0500

    changing to apps

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-# from pages.Contact_info import home
-# import page.Humanizer as Humanizer
 
-# from .Contact_info import home
-# import page.Wikipedia as wiki
 with st.sidebar:
     selected = option_menu(
         menu_title = "Content Writers",
